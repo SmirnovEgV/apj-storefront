3d7cc894ee688d79933895be14b1874587c4ef06
Egor Smirnov <exsplorator@gmail.com>
2025-02-25 22:55:38 -0700

    It works. Im tired of dealig with it as well.

---
+++ tutorial8-1/src/test/java/com/example/tutorial81/Tutorial81ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorial81;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutorial81ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
